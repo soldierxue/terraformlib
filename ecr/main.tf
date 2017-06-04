6e9ccf08c0b5dc676710d6cf48643b082076a308
soldierxue <sys@soldierxue.cn>
2017-06-04 11:33:22 +0800

    add ecr module

---
+++ ecr/main.tf
@@ -0,0 +1,32 @@
+/**
+ * The ECR module aims to create a elastic container registry when needed
+ *
+ * Usage:
+ *
+ *    module "m_ecr" {
+ *      source      = "github.com/soldierxue/terraformlib/code-commit"
+ *      name        = "my-repo"
+ *    }
+ *
+ */
+
+variable name {
+   default ="ecr-repo"
+}
+
+resource "aws_ecr_repository" "ecr_reg" {
+  name = "${var.name}}"
+}
+
+
+output ecr_repo_url {
+  value = "${aws_ecr_repository.ecr_reg.repository_url}"
+}
+
+output ecr_name {
+  value = "${aws_ecr_repository.ecr_reg.name}"
+}
+
+output ecr_arn {
+  value = "${aws_ecr_repository.ecr_reg.arn}"
+}
