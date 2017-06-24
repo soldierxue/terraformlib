1300adff318db06938ddbcb73a5d713fb6625e5c
soldierxue <sys@soldierxue.cn>
2017-06-24 23:09:43 +0800

    add output

--- alb_tgs/outputs.tf
+++ alb_tgs/outputs.tf
@@ -1,6 +1,9 @@
 output "alb_public_url" {
   value = "${aws_alb.dmz-alb.dns_name}:${aws_alb_listener.instance_listener.port}"
 }
+output "alb_public_url_withoutport" {
+  value = "${aws_alb.dmz-alb.dns_name}"
+}
 
 output "alb_listener_arn" {
   value = "${aws_alb_listener.instance_listener.arn}"
